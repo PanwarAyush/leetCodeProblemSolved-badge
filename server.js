f6d53abde8d4a1734a8e417abdfbac6a3b1e3e62
Ayush Panwar <ayushpanwar691@gmail.com>
2023-01-19 19:47:56 +0530

    done

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const http = require('http');
-async function t(){
-const api_url='https://leetcode-stats-api.herokuapp.com/panwar2001';
+var url = require('url');
+async function t(api_url){
 const response = await fetch(api_url);
 const data =  await response.json();
-const s=`<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg  transform="scale(1.9)" width="500px" height="500px" version="1.1" xmlns="http://www.w3.org/2000/svg"><text x="150" y="223" id='num'>${data.totalSolved}</text></svg>`
-return s;
+return data.totalSolved;
 }
 let problems;
-t().then((data)=>{
-problems=data;
+function setProblems( username){
+  const api_url='https://leetcode-stats-api.herokuapp.com/'+username;
+ t(api_url).then((data)=>{
+const s=`<svg width="600" height="100" xmlns="http://www.w3.org/2000/svg"><image href="https://leetcode.com/static/images/LeetCode_Sharing.png" height="100" width="200"/><text x="10" y="18" fill="red" style="background-color:">Solved ${data} problems</text></svg>`
+problems=s;
 })
+}
 
 http.createServer(function (req, res) {
+var url_parts = url.parse(req.url, true);
+var query = url_parts.query;
+  setProblems(query.profile)
   res.setHeader('Content-Type', 'image/svg+xml');
   res.end(problems); 
 }).listen(8080);
